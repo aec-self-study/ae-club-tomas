8cf2aff9a8a843d81e3a05908398a7eddb25156b
Tomas Carvalho <tomas.jpeg@gmail.com>
2023-10-19 19:25:05 +0000

    change orders alias

--- customers.sql
+++ customers.sql
@@ -2,12 +2,12 @@ select
   customers.id, 
   customers.name,
   customers.email,
-  min(o.created_at) as first_order_at,
-  count(o.id) as number_of_orders
+  min(orders.created_at) as first_order_at,
+  count(orders.id) as number_of_orders
 from 
   `analytics-engineers-club.coffee_shop.customers` customers
-  join `analytics-engineers-club.coffee_shop.orders` o
-    on customers.id = o.customer_id
+  join `analytics-engineers-club.coffee_shop.orders` orders
+    on customers.id = orders.customer_id
 group by
   1,2,3
 order by
