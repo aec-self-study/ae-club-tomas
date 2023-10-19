598e4a791fcdf453ac1a05041797917095310659
Tomas Carvalho <tomas.jpeg@gmail.com>
2023-10-19 19:09:28 +0000

    (feat): add customers sql

---
+++ customers.sql
@@ -0,0 +1,16 @@
+select 
+  c.id, 
+  c.name,
+  c.email,
+  min(o.created_at) as first_order_at,
+  count(o.id) as number_of_orders
+from 
+  `analytics-engineers-club.coffee_shop.customers` c
+  join `analytics-engineers-club.coffee_shop.orders` o
+    on c.id = o.customer_id
+group by
+  1,2,3
+order by
+  first_order_at
+limit 5
+
