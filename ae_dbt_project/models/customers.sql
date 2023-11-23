574f41c98fc62f0c9245a17375f81e03684ac1e6
Tomas Carvalho <tomas.jpeg@gmail.com>
2023-11-23 12:25:24 +0000

    feat(dbt project)

---
+++ ae_dbt_project/models/customers.sql
@@ -0,0 +1,15 @@
+{{ config(materialized="table") }}
+
+select
+    customers.id,
+    customers.name,
+    customers.email,
+    min(orders.created_at) as first_order_at,
+    count(orders.id) as number_of_orders
+from `analytics-engineers-club.coffee_shop.customers` customers
+join
+    `analytics-engineers-club.coffee_shop.orders` orders
+    on customers.id = orders.customer_id
+group by customers.id, customers.name, customers.email
+order by first_order_at
+limit 5
